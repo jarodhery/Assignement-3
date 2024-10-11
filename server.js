474dfe5d50e2dbccbe1ef45d615315018bfd03dc
Jarod <jjkhery-chen@myseneca.ca>
2024-10-10 21:10:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const HTTP_PORT = process.env.PORT || 8080;
 const legoData = require('./modules/legoSets');
 
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/views'));
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 
 app.get('/', (req, res) => 
